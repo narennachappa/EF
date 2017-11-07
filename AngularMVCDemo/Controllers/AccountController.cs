ad05790e5260c4e86ad50b6628cee2d1936b056c
narennachappa <narennachappa@yahoo.com>
2017-11-07 16:16:43 +1100

    New changes demo

--- AngularMVCDemo/Controllers/AccountController.cs
+++ AngularMVCDemo/Controllers/AccountController.cs
@@ -7,7 +7,7 @@ using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin.Security;
+using Microsoft.Owin.Security; 
 using AngularMVCDemo.Models;
 
 namespace AngularMVCDemo.Controllers
