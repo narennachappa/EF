1097cb9470645d59571e9dc38b16b64db26a94ae
narennachappa <narennachappa@yahoo.com>
2017-11-07 16:22:37 +1100

    Add project files.

---
+++ EFSample/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace EFSample.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ EFSample/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EFSample
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (TECHNETEntities db = new TECHNETEntities())
+            {
+                Articoli art = db.Articolis.Where((x) => x.CodArt == "ART001").FirstOrDefault();
+
+                txtSample.Text = art.DesArt;
+            }
+        }
+    }
+}
